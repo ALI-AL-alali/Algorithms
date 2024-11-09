1d553fac3c66614cabc56d82d7834a6fa293f443
ALI_ALAli <alialali13484@gmail.com>
2024-11-09 17:27:41 +0300

    Program.cs

--- Hanoi/Hanoi/Program.cs
+++ Hanoi/Hanoi/Program.cs
@@ -13,14 +13,6 @@ class Program
             Hanoi_R(n - 1, B, C, A);
         }
     }
-    static void Hanoi_I(int n, char A, char C, char B)
-    {
-
-        for (int i = 1; i <= (n * n) - 1; i++) 
-        {
-
-        }
-    }
     static void Main()
     {
         int n = 3;
