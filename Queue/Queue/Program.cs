a3407a132d93fc4dbffb9ee1a53bd32464ddeef8
ALI_ALAli <alialali13484@gmail.com>
2024-11-09 17:49:23 +0300

    Add files via upload

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Queue;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<< HELLO >>>");
+            Queue queue = new Queue();
+            int x;
+            do
+            {
+                Console.Write("Enter The Number : \n" +
+                    "1.Add Node in The Queue\n" +
+                    "2.Remove[Delete] Node in The Queue\n" +
+                    "3.Print Queue\n" +
+                    "4.EXIT\n" +
+                    "==> ");
+                x = int.Parse(Console.ReadLine());
+                switch (x)
+                {
+                    case 1:
+                        Console.WriteLine("============{ Add In Queue }============");
+                        Node node = new Node();
+                        Console.Write("Enter The Number : ");
+                        int num = int.Parse(Console.ReadLine());
+                        node.Data = num;
+                        queue.EnQueue(node);
+                        Console.WriteLine("=========================================");
+                        break;
+                    case 2:
+                        Console.WriteLine("==========={ Remove In Queue }===========");
+                        Console.WriteLine($"The Number Has Deleted {queue.First.Data}");
+                        queue.DeQueue();
+                        Console.WriteLine("=========================================");
+                        break;
+                    case 3:
+                        Console.WriteLine("==========={ Print Data Queue }==========");
+                        queue.Print();
+                        Console.WriteLine("=========================================");
+                        break;
+                }
+            } while (x < 4);
+        }
+    }
+}
---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class Queue
+    {
+        int Count = 0;  
+        public Node First { get; set; }
+        public Node Last { get; set; }
+        public Queue() { }
+        public Queue(Node first)
+        {
+            First = first;
+        }
+        public Queue(Node first, Node last)
+        {
+            First = first;
+            Last = last;
+        }
+        public void EnQueue(Node node)
+        {
+            if(First == null)
+            {
+                First = node;
+                Last = node;
+            }
+            Last.Next = node;
+            Last = node;
+            Count++;
+        }
+        public void DeQueue()
+        {
+            if(First == null)
+            {
+                Console.WriteLine("Queue Is Empty");
+                return;
+            }
+            First = First.Next;
+            Count--;
+        }
+        public void Print()
+        {
+            Node node = First;
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(node.Data + "\t");
+                node = node.Next;
+            }
+        }
+    }
+}
