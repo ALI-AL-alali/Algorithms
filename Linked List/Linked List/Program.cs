1a047699e9de3fe9013b730572c30c979e865406
ALI_ALAli <alialali13484@gmail.com>
2024-11-15 13:44:07 +0300

    Program.cs

--- Linked List/Linked List/Program.cs
+++ Linked List/Linked List/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Linked_List
 {
@@ -6,45 +7,84 @@ namespace Linked_List
     {
         static void Main(string[] args)
         {
-            Linked_List linkedList = new Linked_List();
-            // إضافة عناصر في البداية
-            linkedList.AddFirst(10);
-            linkedList.AddFirst(20);
-            linkedList.AddFirst(30);
+            Linked_List list = new Linked_List();
 
-            Console.WriteLine("The linked list after adding elements from the beginning :");
-            linkedList.Print();
+            // إضافة عناصر إلى القائمة
+            Console.WriteLine("Add Node to beginning of list:");
+            list.AddFirst(10);
+            list.AddFirst(40);
+            list.AddFirst(30);
+            list.AddFirst(50);
+            Node node = new Node(55);
+            list.AddFirst(node);
+            list.Print();
+
+            // طباعة بشكل عكسي 
+            Console.WriteLine("\nPrint the list in reverse:");
+            list.PrintReverse(node);
+            Console.WriteLine();
 
             // إضافة عناصر في النهاية
-            linkedList.AddLast(40);
-            linkedList.AddLast(50);
-
-            Console.WriteLine("The linked list after adding elements from the end :");
-            linkedList.Print();
+            Console.WriteLine("\nAdd Node to the end of the list:");
+            list.AddLast(20);
+            list.AddLast(60);
+            list.Print();
 
             // إدراج عنصر في موقع محدد
-            linkedList.InsertIn(25, 2);
+            Console.WriteLine("\nAdd Node at specific location (add 25 at location 2):");
+            list.AddByIndex(25, 2);
+            list.Print();
 
-            Console.WriteLine("Linked list after adding an element at a specific location :");
-            linkedList.Print();
+            // فرز اللائحة 
+            Console.WriteLine("\nSorting the list:");
+            list.Sorting();
+            list.Print();
+
+            // إدراج عنصر في موقع صحيح
+            Console.WriteLine("\nAdd a node(35) in the right place: ");
+            list.AddByData(35);
+            list.Print();
+
+            // بحث عن عنصر معين    
+            Console.WriteLine("\nSearch for Node (25):");
+            int index = list.Serach(25);
+            if (index != -1)
+                Console.WriteLine("Node 25 is at location: " + index);
+            else
+                Console.WriteLine("Node 25 is not in the list.");
+
+            // بحث عن عنصر معين    
+            Console.WriteLine("\nSearch for Node (11):");
+            int i = list.Serach(11);
+            if (i != -1)
+                Console.WriteLine("Node 11 is at location: " + index);
+            else
+                Console.WriteLine("Node 11 is not in the list.");
 
             // حذف العنصر الأول
-            linkedList.DeleteFirst();
-
-            Console.WriteLine("Linked list after deleting an element from the beginning :");
-            linkedList.Print();
+            Console.WriteLine("\nDelete first Node:");
+            list.DeleteFirst();
+            list.Print();
 
             // حذف العنصر الأخير
-            linkedList.DeleteLast();
+            Console.WriteLine("\nDelete last Node:");
+            list.DeleteLast();
+            list.Print();
 
-            Console.WriteLine("Linked list after deleting an element from the end :");
-            linkedList.Print();
+            // حذف العنصر حسب الموقع
+            Console.WriteLine("\nDelete Node at specific location (delete Node at location 2):");
+            list.DeleteByIndex(2);
+            list.Print();
 
             // حذف عنصر حسب البيانات
-            linkedList.DeleteByData(25);
+            Console.WriteLine("Delete node(25) according to the data:");
+            list.DeleteByData(25);
+            list.Print();
 
-            Console.WriteLine("Linked list after deleting an item from a specific location :");
-            linkedList.Print();
+            // افراغ اللائحة 
+            Console.WriteLine("\nClear entire list:");
+            list.Clear();
+            list.Print();
         }
     }
 }
