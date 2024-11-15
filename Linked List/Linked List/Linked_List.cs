1c2eac9034c1496a2272905e09e480e1081d3d9e
ALI_ALAli <alialali13484@gmail.com>
2024-11-15 13:42:21 +0300

    Linked_List.cs

--- Linked List/Linked List/Linked_List.cs
+++ Linked List/Linked List/Linked_List.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -34,7 +34,7 @@ namespace Linked_List
         }
         public void AddLast(int Data)
         {
-            if (first == null)
+            if (IsEmpty())
             {
                 AddFirst(Data);
                 return;
@@ -48,18 +48,18 @@ namespace Linked_List
         }
         public void AddLast(Node Data)
         {
-            if (first == null)
+            if (IsEmpty())
             {
                 AddFirst(Data);
                 return;
             }
-            Node move = first;
-            while (move.Next != null)
-                move = move.Next;
-            move.Next = Data;
+            Node temp = first;
+            while (temp.Next != null)
+                temp = temp.Next;
+            temp.Next = Data;
             count++;
         }
-        public void InsertIn(int Data, int loc)
+        public void AddByIndex(int Data, int loc)
         {
             Node node = new Node(Data);
             if (loc > count || loc < 0)
@@ -86,7 +86,23 @@ namespace Linked_List
             move.Next = node;
             count++;
         }
-        public bool IsEmpty() { return first == null; }
+        public void AddByData(int data)
+        {
+            if (IsEmpty())
+            {
+                AddFirst(data);
+                return;
+            }
+            Node move = new Node(data);
+            Node temp = first;
+            while (temp.Next != null && temp.Next.Data < data)
+            {
+                temp = temp.Next;
+            }
+            move.Next = temp.Next;
+            temp.Next = move;
+            count++;
+        }
         public void DeleteFirst()
         {
             if (IsEmpty())
@@ -118,6 +134,31 @@ namespace Linked_List
             del.Next = null;
             count--;
         }
+        public void DeleteByIndex(int index)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty Linked List!!");
+                return;
+            }
+            if (index < 0 || index >= count)
+            {
+                Console.WriteLine("Index is longer than list length!!");
+                return;
+            }
+            if (index == 0)
+            {
+                DeleteFirst();
+                return;
+            }
+            Node temp = first;
+            for (int i = 0; i < index - 1; i++)
+            {
+                temp = temp.Next;
+            }
+            temp.Next = temp.Next.Next;
+            count--;
+        }
         public void DeleteByData(int data)
         {
             if (IsEmpty())
@@ -146,17 +187,82 @@ namespace Linked_List
             curr.Next = null;
             count--;
         }
-
+        public int Serach(int data)
+        {
+            if (IsEmpty())
+            {
+                return -1;
+            }
+            Node temp = first;
+            int index = 0;
+            while (temp != null)
+            {
+                if (temp.Data == data)
+                {
+                    return index;
+                }
+                temp = temp.Next;
+                index++;
+            }
+            return -1;
+        }
+        public void Sorting()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty Linked List!!");
+                return;
+            }
+            for(Node x = first; x != null; x = x.Next)
+            {
+                for(Node y = x; y != null; y = y.Next)
+                {
+                    if(x.Data > y.Data)
+                    {
+                        int temp = x.Data;
+                        x.Data = y.Data;
+                        y.Data = temp;
+                    }
+                }
+            }
+        }
+        public bool IsEmpty()
+        {
+            return first == null; 
+        }
         public void Print()
         {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty Linked List!!");
+                return;
+            }
             Node temp = first;
             while (temp != null)
             {
-                Console.Write("  =>  " + temp.Data);
+                Console.Write($"=>  {temp.Data}  ");
                 temp = temp.Next;
             }
             Console.WriteLine();
         }
+        public void PrintReverse(Node temp)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty Linked List!!");
+                return;
+            }
+            if(temp == null)
+            {
+                return;
+            }
+                PrintReverse(temp.Next);
+                Console.Write($"  {temp.Data}  <=");
+        }
+        public void Clear()
+        {
+            first = null;
+            count = 0;
+        }
     }
-
 }
